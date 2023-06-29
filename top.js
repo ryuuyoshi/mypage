a0bd81663fe03f672d2b677baaec314ca6bc3967
ryuuyoshi <111184844+ryuuyoshi@users.noreply.github.com>
2023-06-29 19:02:44 +0900

    20230629 acord ok

--- top.js
+++ top.js
@@ -1,36 +1,20 @@
 
 $(function(){
   /* ここにjQueryのコードを書く */
-  
-  $(".pick").click(function(){
-    var $ans = $(this).find(".answer");
-    var $span = $(this).find("span")
+$(".pick").click(function(){
+  var $ans = $(this).find(".answer");
+  var $span = $(this).find("span")
 
-    if($ans.hasClass("close")){
-      $ans.removeClass("close");
-      $ans.slideUp();
-      $span.html("+");
-    } else {
-      $ans.addClass("close");
-      $ans.slideDown();
-      $span.html("-");
-      };
-  });
-
-  $('.fake').mouseover(
-    function(){
-    $(this).attre('src','アイコン\kaisya_komaru_png.png');
-    $('.main-left').text('mitukatta!!');
-  },
-    function(){
-      $(this).attre('src','アイコン\影絵.png');
-      $('.main-left').text('イメージ画像');
-    }
-  
-  );
-
+  if($ans.hasClass("close")){
+    $ans.removeClass("close");
+    $ans.slideUp();
+    $span.html("+");
+  } else {
+    $ans.addClass("close");
+    $ans.slideDown();
+    $span.html("-");
+    };
 });
 
 
-
-アイコン\kaisya_komaru_png.png
+});
